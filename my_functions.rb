2a0854c4b14346af602d1f80c68016b93e00c866
Chris Hunter <orkem2@gmail.com>
2017-06-28 15:31:48 +0100

    Added sum_array function

--- my_functions.rb
+++ my_functions.rb
@@ -1,3 +1,11 @@
 def add_array_lengths(array1,array2)
   return array1.size+array2.size
+end 
+
+def sum_array(array)
+  total = 0
+  for number in array do
+    total +=  number
+  end
+  return total
 end
